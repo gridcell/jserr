e11eccb24ed2d5c675beab40fe78db76d324b4ee
Kim Fisher <saanobhaai@users.noreply.github.com>
2018-02-09 17:17:28 -0500

    Update urls.py
    1.11 compat

--- jserr/urls.py
+++ jserr/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
+import jserr.views
 
-urlpatterns = ["",
-    url(r"^log/$", "jserr.views.log", name="log_js_error"),
+urlpatterns = [
+    url(r"^log/$", jserr.views.log, name="log_js_error"),
 ]
